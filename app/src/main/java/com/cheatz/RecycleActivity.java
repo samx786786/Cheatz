3148923e89c70563451f417ca146960395bf733f
syed abdul muqeeth <samx123786@gmail.com>
2021-07-19 22:10:11 +0530

    Updates 2.3

--- app/src/main/java/com/cheatz/RecycleActivity.java
+++ app/src/main/java/com/cheatz/RecycleActivity.java
@@ -1,15 +1,25 @@
 package com.cheatz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RecycleActivity extends AppCompatActivity {
 
     EditText name,contact,address;
     Button recycle;
+    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +28,32 @@ public class RecycleActivity extends AppCompatActivity {
         name=findViewById(R.id.editTextTextPersonName2);
         contact=findViewById(R.id.editTextTextPersonName3);
         address=findViewById(R.id.editTextTextPersonName4);
+        firestore = FirebaseFirestore.getInstance();
         recycle=findViewById(R.id.button4);
         recycle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // do the push logic here
+
+                String username=name.getText().toString();
+                String contactnumber=contact.getText().toString();
+                String useraddress=address.getText().toString();
+
+                Map<String, String> userMap = new HashMap<>();
+                userMap.put("name", username);
+                userMap.put("contact",contactnumber);
+                userMap.put("address",useraddress);
+                firestore.collection("Recycle").add(userMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+
+                        // to the thinggggg
+
+
+                    }
+                });
+
+
+
             }
         });
     }
--- app/src/main/java/com/cheatz/TestActivity.java
+++ app/src/main/java/com/cheatz/TestActivity.java
@@ -1,10 +1,7 @@
 package com.cheatz;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -13,16 +10,7 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-        Button button=findViewById(R.id.button3);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-
-
-            }
-        });
 
     }
 }
